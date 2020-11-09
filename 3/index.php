28a601cde9e22b343263251d7f7262d6ed1ad1c5
MEHAIGNI Yacine <71050808+yacinemehai@users.noreply.github.com>
2020-11-09 18:06:26 +0100

    Add files via upload

---
+++ 3/index.php
@@ -0,0 +1,31 @@
+
+<?php
+require_once 'Vehicle.php';
+require_once 'Bicycle.php';
+require_once 'Car.php';
+require_once 'Truck.php';
+require_once 'MotorWay.php';
+require_once 'HighWay.php';
+require_once 'ResidentialWay.php';
+require_once 'PedestrianWay.php';
+require_once 'LightableInterface.php';
+
+$bicycle = new Bicycle('blue', 1);
+
+
+$car = new Car('green', 4, 'electric');
+
+
+$truck = new Truck('blue', 5, 'electric', 50);
+
+$MotorWay = new MotorWay();
+
+$ResidentialWay = new ResidentialWay();
+
+$PedestrianWay = new PedestrianWay();
+
+$MotorWay->addVehicle($car);
+
+$PedestrianWay->addVehicle($bicycle);
+
+
